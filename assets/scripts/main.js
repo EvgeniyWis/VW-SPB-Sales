ead40bf4864eb72949ed5cdbafddeb6e598492cc
Evgeniy <KworkEvgeniy@yandex.ru>
2024-04-01 23:15:58 +0300

    Добавлена секция "Акции" и весь оставшийся функционал.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,6 +6,10 @@ const sales__tabs__item__all = document.getElementById("sales__tabs__item__all")
 const sales__tabs__item__customers = document.getElementById("sales__tabs__item__customers");
 const sales__tabs__item__service = document.getElementById("sales__tabs__item__service");
 const sales__tabs__item__trade_in = document.getElementById("sales__tabs__item__trade_in");
+const sales__all = document.getElementById("sales__all");
+const sales__customers = document.getElementById("sales__customers");
+const sales__service = document.getElementById("sales__service");
+const sales__trade_in = document.getElementById("sales__trade_in");
 
 
 /* Открытие и закрытие бургер меню */
@@ -20,14 +24,20 @@ navbar__adaptive_menu_cross.addEventListener("click", () => {
 })
 
 
-/* Нажатие на табы в разделе "Акции" */
+/* Нажатие на табы в разделе "Акции" и переключение секций */
 const sales__tabs__items = [sales__tabs__item__all, sales__tabs__item__customers, sales__tabs__item__service, sales__tabs__item__trade_in];
 let sales__tabs__item__active = sales__tabs__item__all;
+
+const sales__sections = [sales__all, sales__customers, sales__service, sales__trade_in];
 
 for (let sales__tabs__item of sales__tabs__items) {
     sales__tabs__item.addEventListener("click", () => {
         sales__tabs__item.classList.add("sales__tabs--item--active");
         sales__tabs__item__active.classList.remove("sales__tabs--item--active");
+
+        sales__sections[sales__tabs__items.indexOf(sales__tabs__item)].classList.add("sales--active");
+        sales__sections[sales__tabs__items.indexOf(sales__tabs__item__active)].classList.remove("sales--active");
+
         sales__tabs__item__active = sales__tabs__item;
     })
 }
